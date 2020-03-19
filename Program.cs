2ae9bd70cd0888de15426a9b5f484f9b04ecbdfc
Kacper Tonia <KACPER650@bailliegifford.com>
2020-03-19 14:30:43 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Reflection;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace bare_mediatr
+{
+    public class HelloQuery : IRequest<string> { }
+    public class HelloQueryHandler : RequestHandler<HelloQuery, string>
+    {
+        protected override string Handle(HelloQuery request)
+            => "Hello world!";
+    }
+    
+    public class SampleCommand : IRequest { }
+    public class SampleCommandHandler : RequestHandler<SampleCommand>
+    {
+        protected override void Handle(SampleCommand request)
+        {
+            Console.WriteLine("Command handled.");
+        }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mediator = BuildMediator();
+            Run(mediator).Wait();
+        }
+
+        static async Task Run(IMediator mediator)
+        {
+            var result = await mediator.Send(new HelloQuery());
+            Console.WriteLine(result);
+
+            await mediator.Send(new SampleCommand());
+        }
+
+        static IMediator BuildMediator()
+        {
+            var services = new ServiceCollection()
+                .AddMediatR(Assembly.GetEntryAssembly())
+                .BuildServiceProvider();
+
+            return services.GetService<IMediator>();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# bare_mediatr
+
+A minimalist introduction to MediatR, with a pinch of CQRS.
